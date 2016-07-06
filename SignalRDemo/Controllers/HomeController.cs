c754fd84a8d021d6d5aebb8cfe85893ab0449448
Avrumi Friedman <info@lakewoodprogramming.com>
2016-07-05 20:30:32 -0400

    Initial commit

---
+++ SignalRDemo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void NewMessage(string text)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<SimpleHub>();
+            context.Clients.All.newMessageReceived(new {foo = text});
+        }
+
+    }
+}
---
+++ SignalRDemo/Scripts/index.js
@@ -0,0 +1,12 @@
+﻿$(function() {
+    var simpleHub = $.connection.simpleHub;
+    $.connection.hub.start();
+
+    simpleHub.client.newMessageReceived = function(message) {
+        $("ul").append("<li>" + message.foo + "</li>");
+    }
+
+    $("#sendit").on('click', function() {
+        $.post('/home/newmessage', { text: $("#textbox").val() });
+    });
+});
